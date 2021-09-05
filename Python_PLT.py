07b12553d33d44584ae2551c3af0ea3b972db84f
quaterhalfbro <eugeny.pakhalyuk@yandex.ru>
2021-09-05 21:59:52 +0300

    rewrite plt num 3 centers generation

--- Python_PLT.py
+++ Python_PLT.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import pandas as pd
+from random import randint
 
 def plt1():
     m = np.random.sample(200)  # 1
@@ -20,16 +21,22 @@ def plt1():
     plt.show()
 
 def plt3():
-    cl1_x = np.array([10 + (np.random.sample() - 0.5) * 5 for i in range(50)])
-    cl1_y = np.array([5 + (np.random.sample() - 0.5) * 5 for i in range(50)])
-    cl2_x = np.array([2.5 + (np.random.sample() - 0.5) * 5 for i in range(50)])
-    cl2_y = np.array([2.5 + (np.random.sample() - 0.5) * 5 for i in range(50)])
-    cl3_x = np.array([7 + (np.random.sample() - 0.5) * 5 for i in range(50)])
-    cl3_y = np.array([8 + (np.random.sample() - 0.5) * 5 for i in range(50)])
+    cl1_x_center = randint(0, 10)
+    cl1_y_center = randint(0, 10)
+    cl2_x_center = randint(0, 10)
+    cl2_y_center = randint(0, 10)
+    cl3_x_center = randint(0, 10)
+    cl3_y_center = randint(0, 10)
+    cl1_x = np.array([cl1_x_center + (np.random.sample() - 0.5) * 5 for i in range(50)])
+    cl1_y = np.array([cl1_y_center + (np.random.sample() - 0.5) * 5 for i in range(50)])
+    cl2_x = np.array([cl2_x_center + (np.random.sample() - 0.5) * 5 for i in range(50)])
+    cl2_y = np.array([cl2_y_center + (np.random.sample() - 0.5) * 5 for i in range(50)])
+    cl3_x = np.array([cl3_x_center + (np.random.sample() - 0.5) * 5 for i in range(50)])
+    cl3_y = np.array([cl3_y_center + (np.random.sample() - 0.5) * 5 for i in range(50)])
     plt.scatter(cl1_x, cl1_y, c='red')
     plt.scatter(cl2_x, cl2_y, c='blue')
     plt.scatter(cl3_x, cl3_y, c='yellow')
     plt.show()
 
 
-plt1()
+plt3()
